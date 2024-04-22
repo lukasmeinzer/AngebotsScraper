0c6ee804d3c61b9fc355ece3f35f913c99893a50
lukas <lukas.meinzer.98@web.de>
2024-04-22 17:00:46 +0000

    Korrektur Commit zuvor

--- main.py
+++ main.py
@@ -5,18 +5,18 @@ from Cleaning.CleanScraped import ScrapingAufbereitung
 
 url = readToml("URLs","Startpunkt")
 
-TableName = TableName()
-is_new_week = check_for_new_week(TableName)
+table_name = TableName()
+is_new_week = check_for_new_week(table_name)
 
 if is_new_week:
-    ProdukteCrawler(url, TableName)
-    df_meta, df_angebote = ScrapingAufbereitung(TableName)  
+    ProdukteCrawler(url, table_name)
+    df_meta, df_angebote = ScrapingAufbereitung(table_name)  
     
     # Meta Informationen
     insert_into_db(
         df=df_meta,
         db_name="MarktMeta",
-        table_name=TableName + "_meta", 
+        table_name=table_name + "_meta", 
         type="raw"
     )
     insert_into_db(
@@ -30,7 +30,7 @@ if is_new_week:
     insert_into_db(
         df=df_angebote, 
         db_name="MarktAngebote",
-        table_name=TableName + "_angebote", 
+        table_name=table_name + "_angebote", 
         type="raw"
     )
     insert_into_db(
